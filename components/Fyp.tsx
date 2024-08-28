3de3082f0b7689e62cdfe96688bdddf825ca160b
BinayaShrestha556 <binayastha37b@gmail.com>
2024-08-28 17:23:40 +0545

    attempt to disable fyp caching

--- components/Fyp.tsx
+++ components/Fyp.tsx
@@ -1,28 +1,27 @@
-import axios from "axios";
+export const dynamic = "force dynamic";
+export const revalidate = 0;
+
 import React from "react";
 import Card from "./Card";
- const getData = async () => {
-    try {
-      const res = await axios.get(
-        `${process.env.NEXT_PUBLIC_TEST}/video/all-videos`,
-   {headers: {
-    'Cache-Control': 'no-cache, no-store, must-revalidate',
-    'Pragma': 'no-cache',
-    'Expires': '0',
-  },}
-      );
-      // console.log(res.data.data);
+const getData = async () => {
+  try {
+    const { signal } = new AbortController();
 
-      return res.data.data;
-    } catch (error) {
-     
-      console.log(error);
-    }
-  };
-const Fyp =async () => {
- 
- 
-const data=await getData()
+    const response = await fetch(
+      `${process.env.NEXT_PUBLIC_TEST}/video/all-videos`,
+      { cache: "no-store" }
+    );
+    // console.log(res.data.data);
+    const res = await response.json();
+
+    return res.data;
+  } catch (error) {
+    console.log(error);
+  }
+};
+const Fyp = async () => {
+  const data = await getData();
+  // console.log(data)
   return (
     <div className="grid gap-x-3 gap-y-10 grid-cols-1 tablet:grid-cols-3 laptop:grid-cols-4 desktop:grid-cols-5  pb-24 p-2">
       {data.map((e: any, i: any) => (
