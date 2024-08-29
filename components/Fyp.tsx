59d82b80a95c75c6d2ef0ad6dcafd42d9b462571
BinayaShrestha556 <binayastha37b@gmail.com>
2024-08-29 10:24:46 +0545

    fixing no store cache

--- components/Fyp.tsx
+++ components/Fyp.tsx
@@ -1,30 +1,19 @@
-export const dynamic = "force dynamic";
-export const revalidate = 0;
-
 import React from "react";
 import Card from "./Card";
 const getData = async () => {
-  try {
-    const { signal } = new AbortController();
-
-    const response = await fetch(
-      `${process.env.NEXT_PUBLIC_TEST}/video/all-videos`,
-      { cache: "no-store" }
-    );
-    // console.log(res.data.data);
-    const res = await response.json();
-
-    return res.data;
-  } catch (error) {
-    console.log(error);
-  }
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_TEST}/video/all-videos`,
+    { cache: "no-store" }
+  );
+  const data = await response.json();
+  return data.data;
 };
 const Fyp = async () => {
   const data = await getData();
   // console.log(data)
   return (
     <div className="grid gap-x-3 gap-y-10 grid-cols-1 tablet:grid-cols-3 laptop:grid-cols-4 desktop:grid-cols-5  pb-24 p-2">
-      {data.map((e: any, i: any) => (
+      {data&& data.map((e: any, i: any) => (
         <Card
           avatar={e.owner.avatar}
           thumbnail={e.thumbnail}
